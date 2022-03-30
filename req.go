4cef42cb5553595bd385f0d7c816b48889baad70
reus <reusee@gmail.com>
2022-03-30 17:26:06 +0800

    update Req argument types

--- req.go
+++ req.go
@@ -10,6 +10,11 @@ type Method string
 
 type Addr string
 
-func Req(ctx context.Context, method Method, addr Addr) dscope.Scope {
-	return dscope.New(&ctx, &method, &addr).Fork(Defs...)
+func Req(ctx context.Context, method string, addr string) dscope.Scope {
+	return dscope.New(
+		&ctx,
+		func() (Method, Addr) {
+			return Method(method), Addr(addr)
+		},
+	).Fork(Defs...)
 }
