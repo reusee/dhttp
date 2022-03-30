aa54e8fd134e310cefd114577750346f41ed14f9
reus <reusee@gmail.com>
2022-03-30 17:20:26 +0800

    set default retry duration to 0

--- retry.go
+++ retry.go
@@ -7,7 +7,7 @@ type RetryDuration time.Duration
 type RetryDeadline time.Time
 
 func (_ Def) RetryDuration() RetryDuration {
-	return RetryDuration(time.Second * 19)
+	return 0
 }
 
 func (_ Def) RetryDeadline(
